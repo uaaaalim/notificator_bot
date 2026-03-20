a96a9e52f682d005e86eef689fafa89d463a943d
Alim Sultangaliyev <114253420+uaaaalim@users.noreply.github.com>
2026-03-20 15:25:40 +0500

    Consolidate entities mapping and add repositories

--- database/entities/subscribers.py
+++ database/entities/subscribers.py
@@ -1,10 +1,33 @@
-from sqlalchemy import Column, BigInteger
+from sqlalchemy import BigInteger, Column, ForeignKey, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from core.database.base import Base
 from core.database.base import BaseEntity
+
+
+subscriber_stream_topics = Table(
+    "subscriber_stream_topics",
+    Base.metadata,
+    Column(
+        "subscriber_id",
+        BigInteger,
+        ForeignKey("subscribers.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+    Column(
+        "stream_topic_id",
+        BigInteger,
+        ForeignKey("stream_topics.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+)
 
 
 class SubscriberEntity(BaseEntity):
     __tablename__ = "subscribers"
 
-    tg_id = Column(BigInteger, nullable=False)
-    # topic
+    tg_id: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    stream_topics: Mapped[list["StreamTopicEntity"]] = relationship(
+        secondary=subscriber_stream_topics,
+        back_populates="subscribers",
+    )
